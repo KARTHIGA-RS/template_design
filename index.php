4baca4996913cb7e163a9ae51dd5dbe936832329
KARTHGA RS <rsk2695@gmail.com>
2017-12-12 11:20:39 +0530

    first commit in local repo

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSS | Flexbox</title>
+	<link rel="stylesheet" href="main_style.css">
+</head>
+<body>
+
+	<div class="container">
+		<form action="">
+			<div class="form-row">
+				<label  for="name">Name:</label>
+				<input type="text" name="name" id="name">
+			</div>
+			<div class="form-row">
+				<label for="email">Email.Id:</label>
+				<input type="text" name="email" id="email">
+			</div>
+		</form>
+	</div>
+
+</body>
+</html>
